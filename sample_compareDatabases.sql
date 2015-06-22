2f485065532741a18aad0aeba213b79dcb7e17e6
george.cj <georgecj11@gmail.com>
2015-06-23 00:55:39 +0530

    Sample input file for compareDatabases.py

---
+++ sample_compareDatabases.sql
@@ -0,0 +1,3 @@
+select sql_no_cache * from masters.organizations where id = 10;
+select sql_no_cache * from masters.organizations where id = 100;
+select sql_no_cache * from masters.organizations where id = 103;
